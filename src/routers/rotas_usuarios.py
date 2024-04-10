101bb62163fc8249d144201dc3b31d57deedddcc
jailsonzarur <jailson.zarur@ufpi.edu.br>
2024-04-09 21:00:17 -0300

    ROUTER para usuarios adicionado.

--- src/routers/rotas_usuarios.py
+++ src/routers/rotas_usuarios.py
@@ -1,5 +1,19 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, Depends
 from sqlalchemy.orm import Session
+from src.infra.sqlalchemy.config.database import get_bd
+from src.infra.sqlalchemy.repositories.usuario import RepositorioUsuario
+from src.schema.schemas import Usuario
+from typing import List
 
 router = APIRouter()
 
+
+@router.post('/signup', status_code=status.HTTP_201_CREATED, response_model=Usuario)
+def criar_usuario(usuario: Usuario, db: Session = Depends(get_bd)):
+    usuario_criado = RepositorioUsuario(db).criar(usuario)
+    return usuario_criado
+
+@router.get('/usuarios', response_model=List[Usuario])
+def listar_usuarios(db: Session = Depends(get_bd)):
+    usuarios = RepositorioUsuario(db).listar()
+    return usuarios
--- src/server.py
+++ src/server.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import Session
 from src.infra.sqlalchemy.repositories.usuario import RepositorioUsuario
 from src.schema.schemas import Usuario
 from src.infra.sqlalchemy.config.database import get_bd
-from src.routers import rotas_produtos
+from src.routers import rotas_produtos, rotas_usuarios
 
 origins = ['http://127.0.0.1:8000/']
 
@@ -27,15 +27,5 @@ app = FastAPI()
 #PRODUTOS
 app.include_router(rotas_produtos.router)
 
-
 #USUARIOS
-
-@app.post('/signup', status_code=status.HTTP_201_CREATED, response_model=Usuario)
-def criar_usuario(usuario: Usuario, db: Session = Depends(get_bd)):
-    usuario_criado = RepositorioUsuario(db).criar(usuario)
-    return usuario_criado
-
-@app.get('/usuarios', response_model=List[Usuario])
-def listar_usuarios(db: Session = Depends(get_bd)):
-    usuarios = RepositorioUsuario(db).listar()
-    return usuarios
+app.include_router(rotas_usuarios.router)
